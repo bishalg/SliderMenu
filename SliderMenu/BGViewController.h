4de63dcf3eccddf96cc325fd55e624f703899084
Bishal Ghimire <bghimire@braindigit.com>
2013-09-02 20:02:35 +0545

    Initial Commit

---
+++ SliderMenu/BGViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGViewController.h
+//  SliderMenu
+//
+//  Created by Bishal Ghimire on 9/2/13.
+//  Copyright (c) 2013 Bishal Ghimire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGViewController : UIViewController
+
+@end
---
+++ SliderMenu/BGViewController.m
@@ -0,0 +1,29 @@
+//
+//  BGViewController.m
+//  SliderMenu
+//
+//  Created by Bishal Ghimire on 9/2/13.
+//  Copyright (c) 2013 Bishal Ghimire. All rights reserved.
+//
+
+#import "BGViewController.h"
+
+@interface BGViewController ()
+
+@end
+
+@implementation BGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
